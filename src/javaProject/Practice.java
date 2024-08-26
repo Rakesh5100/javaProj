9c9246ae35730228733d4ee89a43786dff4cad02
rakesh <rakeshraut510@gmail.com>
2024-08-26 23:33:34 +0530

    first branch

--- src/javaProject/Practice.java
+++ src/javaProject/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
 	    public static void main(String []args)
 	    {
 	        System.out.println("My First Java Program.");
+	        System.out.println("This is rakesh.");
 	    }
 	};
 
